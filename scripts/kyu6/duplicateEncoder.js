bf2b997ee9aeb90d5ddb89eaebc86109a6d674b0
Emiliano Menéndez <emystein@gmail.com>
2020-09-24 00:42:03 -0300

    Duplicate Encoder

--- scripts/kyu6/duplicateEncoder.js
+++ scripts/kyu6/duplicateEncoder.js
@@ -1,27 +1,11 @@
 const duplicateEncoder = function (word) {
-  chars = word.toLowerCase().split('');
-
-  positions = [];
-
-  for (currentPosition = 0; currentPosition < chars.length; currentPosition++) {
-    const char = chars[currentPosition];
-
-    if (positions[char] == undefined) {
-      positions[char] = [];
-    }
-
-    positions[char].push(currentPosition);
-  }
-
-  result = [];
-
-  for (currentPosition = 0; currentPosition < chars.length; currentPosition++) {
-    const char = chars[currentPosition];
-    encoded = (positions[char].length > 1) ? ')' : '(';
-    result.push(encoded);
-  }
-
-  return result.join('');
+  return word
+    .toLowerCase()
+    .split('')
+    .map(function (a, i, w) {
+      return w.indexOf(a) == w.lastIndexOf(a) ? '(' : ')'
+    })
+    .join('');
 };
 
 module.exports = duplicateEncoder;
